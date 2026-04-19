bbff39421d2e63c631278c4e63cc15e7a2e8c10f
Bharani010 <sveerepa@cougarnet.uh.edu>
2026-04-18 20:31:15 -0500

    fix: use %.8f format in toVectorString to avoid Java scientific notation breaking pgvector

--- src/main/java/com/paperlineage/vector/PgVectorClient.java
+++ src/main/java/com/paperlineage/vector/PgVectorClient.java
@@ -96,7 +96,7 @@ public class PgVectorClient {
         StringBuilder sb = new StringBuilder("[");
         for (int i = 0; i < embedding.length; i++) {
             if (i > 0) sb.append(",");
-            sb.append(embedding[i]);
+            sb.append(String.format("%.8f", embedding[i]));
         }
         sb.append("]");
         return sb.toString();
